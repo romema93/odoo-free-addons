00786d26420817b4cd6194f193efd95d5c0d97e4
Luis Rodrigo Mejia Mateus <rodriz_20@hotmail.com>
2019-11-28 09:09:16 -0500

    update manifest

--- report_pdf_options/__manifest__.py
+++ report_pdf_options/__manifest__.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Pdf report options',
-    'summary': """Shows options in pdf reports""",
+    'summary': """shows a modal window with options for printing, downloading or opening pdf reports""",
+    'description': """
+        Choose one of the following options when printing a pdf report:
+        - print. print the pdf report directly with the browser
+        - download. download the pdf report on your computer
+        - open. open the pdf report in a new tab
+        You can also set a default options for each report
+    """,
     'author': 'Luis Rodrigo Mejia Mateus',
     'category': 'Productivity',
     'depends': ['report'],
